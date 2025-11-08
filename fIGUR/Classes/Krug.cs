a48792af28554fd5d7057aceeb36368f3ba83a03
qwelord <secondofall22@rambler.ru>
2025-11-08 17:29:04 +0500

    Класс наследник Krug. Чернышков

---
+++ fIGUR/Classes/Krug.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace fIGUR.Classes
+{
+    public class Krug : Treugol
+    {
+        public double Radius { get; set; }
+
+        public Krug(double x, double y, double radius) : base(x, y) {
+        
+            Radius = radius;
+        
+        }
+
+        public void Draw(Canvas canvas)
+        {
+            Ellipse ellipse = new Ellipse
+            {
+                Width = Radius * 2,
+                Height = Radius * 2,
+                Fill = Brushes.Blue
+            };
+
+            Canvas.SetLeft(ellipse, X - Radius);
+            Canvas.SetLeft(ellipse, Y - Radius);
+            canvas.Children.Add(ellipse);
+        }
+    }
+}
