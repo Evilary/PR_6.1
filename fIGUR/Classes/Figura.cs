b897494f6524d4bab6f3cd219cc35ca626925f37
qwelord <secondofall22@rambler.ru>
2025-11-08 17:11:39 +0500

    Базовый класс Figure. Чернышков

---
+++ fIGUR/Classes/Figura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fIGUR.Classes
+{
+    public class Figura
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Figura(double x, double y) {
+        
+            X = x;
+            Y = y;
+        }
+    }
+}
